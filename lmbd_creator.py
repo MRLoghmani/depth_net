b529bfd8596d409d8298b12318d20c5cf79b58c6
engharat <engharat@gmail.com>
2016-03-24 15:09:41 +0100

    mah!

--- lmbd_creator.py
+++ lmbd_creator.py
@@ -39,18 +39,25 @@ else:
     exit()
 with env.begin(write=True) as txn:
     # txn is a Transaction object
-    for i in range(num_files):
-        (im_path, label) = images[i].split()
-        import code
-        code.interact(local=locals()) 
-        datum = caffe.proto.caffe_pb2.Datum()
-        datum.channels = 1
-        datum.height = res
-        datum.width = res
-        img = cv2.imread(join(args.root_dir,im_path), flags)
-        datum.data = img.tobytes()  # or .tostring() if numpy < 1.9
-        datum.label = int(label)
-        str_id = '{:08}'.format(i)
+counter=0
+print "num files:%d" % range(num_files)
+root_dir=args.root_dir
+for i in range(num_files):
+    print "file: %d" % i
+    (im_path, label) = images[i].split()
+   # import code
+   # code.interact(local=locals()) 
+    datum = caffe.proto.caffe_pb2.Datum()
+    datum.channels = 1
+    datum.height = res
+    datum.width = res
+    fullfilepath=string.join(root_dir,im_path)
+    img = cv2.imread(fullfilepath, flags)
+    import code 
+    code.interact(local=locals())
+    datum.data = img.tobytes()  # or .tostring() if numpy < 1.9
+    datum.label = int(label)
+    str_id = '{:08}'.format(i)
 
         # The encode is only essential in Python 3
-        txn.put(str_id.encode('ascii'), datum.SerializeToString())
+    txn.put(str_id.encode('ascii'), datum.SerializeToString())
