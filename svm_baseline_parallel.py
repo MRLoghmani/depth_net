fafacb3e4cb1d91033b4470dbf172be4bf440d12
Fabio Maria Carlucci <fabiom.carlucci@yahoo.it>
2016-07-27 10:42:12 +0200

    deploy

--- svm_baseline_parallel.py
+++ svm_baseline_parallel.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+from sklearn.cross_validation import train_test_split
+from sklearn.grid_search import GridSearchCV
+from sklearn.metrics import classification_report
 import re
 import pickle
 from sklearn.metrics import confusion_matrix
@@ -11,6 +14,16 @@ import time
 from multiprocessing import Process, Array
 import mkl
 from sklearn.decomposition import PCA
+from scipy import io
+
+
+class RunParams:
+    def __init__(self, save_kernel=False, tuneParams=False, pca_dims=None, out_name="Kernel"):
+        self.pca_dims = pca_dims
+        self.save_kernel = save_kernel
+        self.tuneParams = tuneParams
+        self.C = 1
+        self.out_name = out_name
 
 type_regex = re.compile(ur'_([depthcrop]+)\.png')
 
@@ -32,6 +45,8 @@ def get_arguments():
     parser.add_argument("--mkl_threads", type=int, default=2)
     parser.add_argument("--classes", type=int, default=51)
     parser.add_argument("--PCA_dims", type=int, default=None)
+    parser.add_argument("--tuneParams", action="store_true")
+    parser.add_argument("--kernel_name", default="kernel")
     args = parser.parse_args()
     return args
 
@@ -53,13 +68,48 @@ def is_alive(job):
     return status
 
 
-def prepare_jobs(split_dir, features, n_splits, jobs, classes, PCA_dims):
+def prepare_jobs(split_dir, features, n_splits, jobs, classes, runParams):
+    if runParams.tuneParams:
+        print "Running parameter optimization"
+        (X, y) = load_split(join(split_dir, args.split_prefix + 'train_split_0.txt'), features, classes)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+        tuned_parameters = [{'C': [0.0001, 0.001, 0.1, 1, 10, 100, 1000]}]
+        scores = ['precision', 'recall']
+        for score in scores:
+            print("# Tuning hyper-parameters for %s" % score)
+            print()
+
+            clf = GridSearchCV(svm.LinearSVC(dual=False), tuned_parameters, cv=5,
+                               scoring='%s_weighted' % score, n_jobs=8)
+            clf.fit(X_train, y_train)
+
+            print("Best parameters set found on development set:")
+            print()
+            print(clf.best_params_)
+            print()
+            print("Grid scores on development set:")
+            print()
+            for params, mean_score, scores in clf.grid_scores_:
+                print("%0.3f (+/-%0.03f) for %r"
+                      % (mean_score, scores.std() * 2, params))
+            print()
+
+            print("Detailed classification report:")
+            print()
+            print("The model is trained on the full development set.")
+            print("The scores are computed on the full evaluation set.")
+            print()
+            y_true, y_pred = y_test, clf.predict(X_test)
+            print(classification_report(y_true, y_pred))
+            print()
+            runParams.C = clf.best_params_['C']
+
     jobs_todo = []
     jobs_running = []
     splits_acc = Array('d', range(n_splits))
     for i in range(n_splits):
         jobs_todo.append(Process(target=run_split, name="Split" + str(i),
-                                 args=(split_dir, features, n_splits, splits_acc, i, classes, PCA_dims)))
+                                 args=(split_dir, features, n_splits, splits_acc, i, classes, runParams)))
     jobs_todo.reverse()  # just to get the jobs in expected order
     while len(jobs_running) + len(jobs_todo):  # while there are still jobs running or to run
         if len(jobs_todo) and len(jobs_running) < jobs:
@@ -74,51 +124,52 @@ def prepare_jobs(split_dir, features, n_splits, jobs, classes, PCA_dims):
     print np.mean(splits_acc)
 
 
-def run_split(split_dir, features, n_splits, splits_acc, x, classes, PCA_dims):
-    #    import ipdb; ipdb.set_trace()
-    f_size = features[features.keys()[0]].shape[0]
-    print "Loading split %d" % x
-    train_lines = open(join(split_dir, args.split_prefix +
-                            'train_split_' + str(x) + '.txt'), 'rt').readlines()
-    test_lines = open(join(split_dir, args.split_prefix +
-                           'test_split_' + str(x) + '.txt'), 'rt').readlines()
-    # pre allocate space for features
-    training_samples = get_samples_per_class(train_lines, classes)
-    testing_samples = get_samples_per_class(test_lines, classes)
-    train_features = []
-    test_features = []
+def load_split(split_path, feat_dict, classes):
+    f_size = feat_dict[feat_dict.keys()[0]].shape[0]
+    ft_lines = open(split_path, 'rt').readlines()
+    samples = get_samples_per_class(ft_lines, classes)
+    features = []
     for c in range(classes):
-        train_features.append(np.empty((training_samples[c], f_size)))
-        test_features.append(np.empty((testing_samples[c], f_size)))
-    # load the features
+        features.append(np.empty((samples[c], f_size)))
     ccounter = np.zeros(classes, dtype='int')
-    for line in train_lines:
+    for line in ft_lines:
         [path, classLabel] = line.split()
         nClass = int(classLabel)
-        train_features[nClass][ccounter[nClass]] = features[path]
+        features[nClass][ccounter[nClass]] = feat_dict[path]
         ccounter[nClass] += 1
-    ccounter = np.zeros(classes, dtype='int')
-    for line in test_lines:
-        [path, classLabel] = line.split()
-        nClass = int(classLabel)
-        test_features[nClass][ccounter[nClass]] = features[path]
-        ccounter[nClass] += 1
-    train_labels = np.hstack(
-        [np.ones(data.shape[0]) * c for c, data in enumerate(train_features)]).ravel()
-    test_labels = np.hstack(
-        [np.ones(data.shape[0]) * c for c, data in enumerate(test_features)]).ravel()
-    test_features = np.vstack(test_features)
-    train_features = np.vstack(train_features)
+    labels = np.hstack([np.ones(data.shape[0]) * c for c,
+                        data in enumerate(features)]).ravel()
+    features = np.vstack(features)
+    return (features, labels)
+
+
+def run_split(split_dir, features, n_splits, splits_acc, x, classes, runParams):
+    print "Loading split %d" % x
+    (train_features, train_labels) = load_split(join(split_dir,
+                                                     args.split_prefix + 'train_split_' + str(x) + '.txt'), features, classes)
+    (test_features, test_labels) = load_split(join(split_dir,
+                                                   args.split_prefix + 'test_split_' + str(x) + '.txt'), features, classes)
     print "Loaded %s train and %s test - starting svm"
+    save_kernel_matrix(train_features, test_features, train_labels, test_labels, runParams.out_name)
     splits_acc[x] = do_svm(LoadedData(
-        train_features, train_labels, test_features, test_labels), x, PCA_dims)
+        train_features, train_labels, test_features, test_labels), x, runParams)
 
 
-def do_svm(loaded_data, split_n, PCA_dims=None):
+def save_kernel_matrix(train_data, test_data, train_labels, test_labels, out_name):
+    data = {}
+    data["train_kernel"] = train_data.dot(train_data.T)
+    data["test_kernel"] = train_data.dot(test_data.T)
+    data["train_labels"] = train_labels
+    data["test_labels"] = test_labels
+    io.savemat(out_name, mdict=data)
+
+
+def do_svm(loaded_data, split_n, runParams):
     #    loaded_data = load_split(args.input_dir, args.train_file, args.test_file)
     print "Fitting SVM to data - train data %s, test data %s" \
         % (str(loaded_data.train_patches.shape), str(loaded_data.test_patches.shape))
-    if PCA_dims:
+    if runParams.pca_dims:
+        PCA_dims = runParams.pca_dims
         print "Will perform PCA to reduce dimensions to %d" % PCA_dims
         start = time.time()
         pca = PCA(n_components=PCA_dims)
@@ -135,7 +186,7 @@ def do_svm(loaded_data, split_n, PCA_dims=None):
         test_data = loaded_data.test_patches
     print "Feature mean %f and std %f" % (train_data.mean(), train_data.std())
     start = time.time()
-    clf = svm.LinearSVC(dual=False)  # C=0.00001 good for JHUIT
+    clf = svm.LinearSVC(dual=False, C=runParams.C)  # C=0.00001 good for JHUIT
     clf.fit(train_data, loaded_data.train_labels)
     res = clf.predict(test_data)
     confusion = confusion_matrix(loaded_data.test_labels, res)
@@ -198,13 +249,13 @@ if __name__ == '__main__':
     mkl.set_num_threads(args.mkl_threads)
     print "\n"
     print args
-    #import ipdb; ipdb.set_trace()
     conf_path = args.conf_name
     features = get_features(args)
     if features is None:
         print "Features not found or corruped - exiting"
         quit()
+    params = RunParams(tuneParams=args.tuneParams, out_name=args.kernel_name)
     prepare_jobs(args.split_dir, features, args.splits,
-                 args.jobs, args.classes, args.PCA_dims)
+                 args.jobs, args.classes, params)
     elapsed_time = time.time() - start_time
     print " Total elapsed time: %d " % elapsed_time
