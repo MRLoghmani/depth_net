38768ee0eb0899aa548cc9a169615ff29974cb49
engharat <engharat@gmail.com>
2016-08-28 21:33:18 +0200

    fusemargin

--- do_jhuit_tests.sh
+++ do_jhuit_tests.sh
@@ -18,13 +18,13 @@ COLORJ_NAME=${FEAT_FOLDER}jhuit_caffe_${JOB_ID}_colorjet.pkl
 ORIG_NAME=${FEAT_FOLDER}jhuit_caffe_${JOB_ID}_original.pkl
 echo Starting work on $CAFFE_MODEL
 echo Extracting JHUIT normalized to $NORM_NAME, layer $layer
-#python feature_extractor.py /home/hades/Downloads/jhuit_all/JHUIT_normalized/ JHUIT/all_depth.txt $DEPLOY $CAFFE_MODEL $NORM_NAME --center_data --gpu_id 0 --batch-size 1 --layer_name ${layer}
+python feature_extractor.py /home/hades/Downloads/jhuit_all/JHUIT_normalized/ JHUIT/all_depth.txt $DEPLOY $CAFFE_MODEL $NORM_NAME --center_data --gpu_id 0 --batch-size 1 --layer_name ${layer}
 
 #echo Extracting JHUIT colorjet to $COLORJ_NAME
-#python feature_extractor.py ../JHUIT/JHUIT_colorjet/ JHUIT/all_depth.txt $DEPLOY $CAFFE_MODEL $COLORJ_NAME --center_data --batch-size $BSIZE  --layer_name fc6
+python feature_extractor.py ../JHUIT/JHUIT_colorjet/ JHUIT/all_depth.txt $DEPLOY $CAFFE_MODEL $COLORJ_NAME --center_data --batch-size $BSIZE  --layer_name fc6
 
 echo Extracting JHUIT original to $ORIG_NAME
-#python feature_extractor.py /home/hades/Downloads/jhuit_all/JHUIT/ JHUIT/all_depth.txt $DEPLOY $CAFFE_MODEL $ORIG_NAME --center_data --gpu_id 0 --batch-size 1  --layer_name ${layer}
+python feature_extractor.py /home/hades/Downloads/jhuit_all/JHUIT/ JHUIT/all_depth.txt $DEPLOY $CAFFE_MODEL $ORIG_NAME --center_data --gpu_id 0 --batch-size 1  --layer_name ${layer}
 
 echo "Running SVM on $NORM_NAME"
 SECONDS=0
@@ -33,10 +33,10 @@ echo "Took $SECONDS seconds"
 
 #SECONDS=0
 #echo "Running SVM on $COLORJ_NAME"
-#python -u svm_baseline.py JHUIT/ $COLORJ_NAME --splits 1 --split_prefix jhuit_depth_
+python -u svm_baseline.py JHUIT/ $COLORJ_NAME --splits 1 --split_prefix jhuit_depth_
 #echo "Took $SECONDS seconds"
 
 SECONDS=0
 echo "Running SVM on $ORIG_NAME"
-#python -u svm_baseline_parallel.py JHUIT/ $ORIG_NAME --splits 1 --split_prefix jhuit_depth_ --classes 49  --mkl_threads 2 --kernel_name jhuit_orig_kernel
+python -u svm_baseline_parallel.py JHUIT/ $ORIG_NAME --splits 1 --split_prefix jhuit_depth_ --classes 49  --mkl_threads 2 --kernel_name jhuit_orig_kernel
 echo "Took $SECONDS seconds"
