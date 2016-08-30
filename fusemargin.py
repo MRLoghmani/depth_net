852c490daf03abb93bd29a4118711a1706f87ea1
engharat <engharat@gmail.com>
2016-08-30 10:50:47 +0200

    fusemargin 1.0

--- fusemargin.py
+++ fusemargin.py
@@ -32,17 +32,19 @@ LoadedData = namedtuple(
 
 def get_arguments():
     parser = ArgumentParser(
-        description='Fuse two, three or four margins.')
+        description='Fuse two, three or four margins.Please provide test_labels.pkl in the dir')
     #parser.add_argument("split_dir")
     #parser.add_argument("feature_dict", nargs='+',
     #                    help="Can be one or two feature dictionaries")
     #parser.add_argument("--conf_name", default=None,
     #                    help="If defined will save confusions matrices for each split at give output")
+    parser.add_argument("--nSplit", type=int)
     parser.add_argument("--nMargin", type=int, default=2)
     parser.add_argument("--m1", default=None)
     parser.add_argument("--m2", default=None)
     parser.add_argument("--m3", default=None)
     parser.add_argument("--m4", default=None)
+    parser.add_argument("--test_labels",default='test_labels')
     args = parser.parse_args()
     return args
 
@@ -52,36 +54,46 @@ if __name__ == '__main__':
     args = get_arguments()
     print "\n"
     print args
-    test_labels = pickle.load(open('test_labels.pkl'))
-    if args.nMargin == 2:
-        margin1=pickle.load(open(args.m1))
-        margin2=pickle.load(open(args.m2))
-        mean_margin=(margin1+margin2)/2
-    if args.nMargin == 3:
-        margin1=pickle.load(open(args.m1))
-        margin2=pickle.load(open(args.m2))
-        margin3=pickle.load(open(args.m3))
-        mean_margin= (margin1+margin2+margin3)/3
-    if args.nMargin == 4:
-        margin1=pickle.load(open(args.m1))
-        margin2=pickle.load(open(args.m2))
-        margin3=pickle.load(open(args.m3))
-        margin4=pickle.load(open(args.m4))
-        mean_margin= (margin1+margin2+margin3+margin4)/4
-        
-    SamplesNum=len(mean_margin)
-    ClassNum=len(mean_margin[0])
-    res = np.zeros(SamplesNum)
-    for k in range(SamplesNum):
-        res[k]=np.argmax(mean_margin[k])
-    #res[k]=[(np.argmax(mean_margin[k])) for k in range(SamplesNum)]
-    
-#            print k
-#            res[k] = [(i if mean_margin[k][p] > 0 else j) for p,(i,j) in enumerate((i,j) 
-#                                                    for i in range(ClassNum)
-#                                                    for j in range(i+1,ClassNum))]
-    confusion = confusion_matrix(test_labels, res)
-    correct = (res == test_labels).sum()
-    print "Margins fusion  " + str(args.m1) + " and " + str(args.m2) +" got " + str((100.0 * correct) / test_labels.size) \
-            + "% correct"
+    splits_acc=0.0
+    for t in range(args.nSplit):
+#        import code
+#        code.interact(local=locals())
+        test_labels = pickle.load(open(args.test_labels+'_split'+str(t)))
+        if args.nMargin == 2:          
+            file1=args.m1+'_split'+str(t)
+            file2=args.m2+'_split'+str(t)
+            margin1=pickle.load(open(file1))
+            margin2=pickle.load(open(file2))
+            mean_margin=(margin1+margin2)/2
+        if args.nMargin == 3:
+            file1=args.m1+'_split'+str(t)
+            file2=args.m2+'_split'+str(t)
+            file3=args.m3+'_split'+str(t)
+            margin1=pickle.load(open(file1))
+            margin2=pickle.load(open(file2))
+            margin3=pickle.load(open(file3))
+            mean_margin= (margin1+margin2+margin3)/3
+        if args.nMargin == 4:
+            file1=args.m1+'_split'+str(t)
+            file2=args.m2+'_split'+str(t)
+            file3=args.m3+'_split'+str(t)
+            file4=args.m4+'_split'+str(t)
+            margin1=pickle.load(open(file1))
+            margin2=pickle.load(open(file2))
+            margin3=pickle.load(open(file3))
+            margin4=pickle.load(open(file4))
+            mean_margin= (margin1+margin2+margin3+margin4)/4
+        SamplesNum=len(mean_margin)
+        ClassNum=len(mean_margin[0])
+        res = np.zeros(SamplesNum)
+        for k in range(SamplesNum):
+            res[k]=np.argmax(mean_margin[k])
+        #res[k]=[(np.argmax(mean_margin[k])) for k in range(SamplesNum)]
+        confusion = confusion_matrix(test_labels, res)
+        correct = (res == test_labels).sum()
+        print "Margins fusion " + str(args.m1) + " and " + str(args.m2) +" split "+str(t)+" got " + str((100.0 * correct) / test_labels.size) \
+                 + "% correct"
+        splits_acc=splits_acc+((100.0 * correct) / test_labels.size)
+    splits_acc=splits_acc / args.nSplit
+    print "mean accuracy: " + str(splits_acc)
 
